11d29a77fe36e558542bc9deec9068c96e5087cf
Adrien CAILLOL <acaillol@sii.fr>
2019-10-11 17:11:19 +0200

    Renommage des solutions et ajout du pptx

---
+++ LegacyApplication/LegacyApplication/WeatherForecast.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace LegacyApplication
+{
+    public class WeatherForecast
+    {
+        public WeatherForecast(string callType)
+        {
+            Type += callType;
+        }
+
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+
+        public string Type { get; private set; } = "LEGACY_";
+
+
+    }
+}
